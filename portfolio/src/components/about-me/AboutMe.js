c70b1c2b20571d6d20811ea9e2a9eb4f4118a2ef
AngelMond <abraham_will@hotmail.com>
2022-07-31 03:22:57 -0500

    Added text media query for paragraphs in aboutMe section

--- portfolio/src/components/about-me/AboutMe.js
+++ portfolio/src/components/about-me/AboutMe.js
@@ -6,16 +6,16 @@ class AboutMe extends Component {
             <section className='container '>
                 <article  className='section-border mt-5 p-4 shadow'>
 
-                    <h2 className='display-6 fw-bold'>Full Stack Web Developer</h2>
+                    <h2 className='paragraph display-6 fw-bold text-sm-center text-md-start'>Full Stack Web Developer</h2>
 
                     <div id="aboutMe-text-container" className='p-2'>
-                        <p className='fs-4 px-3 mb-4'>
+                        <p className='paragraph fs-4 px-3 mb-4 text-sm-center text-md-start'>
                             I'm from Mexico City and i'm currently enrolled in a bootcamp provided by Tec
                             de Monterrey and Trilogy Education Services where I have had the opportunity to gain experience by
                             developing projects on my own and also projects with different work teams.
                         </p>
 
-                        <p className='fs-4 px-3 mb-4'>
+                        <p className='paragraph fs-4 px-3 mb-4'>
                             I have always had a passion for technology and I had the opportunity to change my career and I do not regret it,
                             it is something incredible because I can sit for hours and hours learning and programming and I have never felt so much
                             passion for something in my life and I am happy to have discovered my passion. It's true when they say:  <cite>"Choose a job
@@ -27,7 +27,7 @@ class AboutMe extends Component {
                             I really like learning about data structures and algorithms. I feel that it is a fundamental part of being a good software developer.
                         </p>
 
-                        <p className='fs-4 px-3 mb-4'>
+                        <p className='paragraph fs-4 px-3 mb-4'>
                             My resume is provided, please feel free to download it, if interested look forward to hearing from you!
                         </p>
                         <p>
