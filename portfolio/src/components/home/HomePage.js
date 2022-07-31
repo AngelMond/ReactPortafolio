7e3126d689d497213282c67b81bd2848f542e33b
AngelMond <abraham_will@hotmail.com>
2022-07-31 03:49:10 -0500

    Added article tag inside a section and added class container for section

--- portfolio/src/components/home/HomePage.js
+++ portfolio/src/components/home/HomePage.js
@@ -4,7 +4,8 @@ import photo from '../../images/my-photo.jpg';
 class HomePage extends Component{
     render(){
         return(
-            <section className='section-border row d-flex flex-wrap homePage mt-5 p-4 shadow  align-items-center'>
+          <section className='container'>
+              <article className='section-border row d-flex flex-wrap homePage mt-5 p-4 shadow  align-items-center'>
                 <div className='col'>
                     <h2 className='display-1 text-center'>Hi, I'm Angel!</h2>
                     <p className='display-6 text-center'>A Pasionate Software Engineer</p>
@@ -12,7 +13,8 @@ class HomePage extends Component{
                 <div className='col d-flex justify-content-center align-items-center'>
                     <img id='myPhoto' className='rounded-circle' src={photo}/>
                 </div>
-            </section>
+            </article>
+          </section>
         )
     }
 }
