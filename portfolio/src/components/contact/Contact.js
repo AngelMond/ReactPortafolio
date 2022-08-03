32d8266fbbd70523ac5098be374a15f8da89c249
AngelMond <abraham_will@hotmail.com>
2022-08-03 00:53:07 -0500

    Contact section created, Needs to add verification for inputs

--- portfolio/src/components/contact/Contact.js
+++ portfolio/src/components/contact/Contact.js
@@ -1,12 +1,41 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-class Contact extends Component{
-    render(){
-        return(
-            <section className="container mt-5">
+class Contact extends Component {
+    render() {
+        return (
+            <section className="container contact-section mt-5">
                 <article className="section-border p-3">
-                    <h3 className="section-headings">Contact</h3>
-
+                    <h3 className="section-headings text-center">Contact</h3>
+                    <div className="row justify-content-center mt-4">
+                        <div className="col-12 col-md-9">
+                            <form action="" className="form" method="post">
+                                <div className="row">
+                                    <div className="col-12 col-md-6 mb-3">
+                                        <div className="form-floating ">
+                                            <input className="form-control" type="text" name="name" placeholder="Name" />
+                                            <label className="" for="name">Name</label>
+                                        </div>
+                                    </div>
+                                    <div className="col-12 col-md-6 mb-3">
+                                        <div className="form-floating ">
+                                            <input className="form-control" type="text" name="email" placeholder="email" />
+                                            <label className="" for="email">email</label>
+                                        </div>
+                                    </div>
+                                    <div className="col-12">
+                                        <div className="form-floating ">
+                                            <textarea className="form-control textarea-contact" name="message" id="message" placeholder="Message">
+                                            </textarea>
+                                            <label for="message">Message</label>
+                                        </div>
+                                        <div className="d-flex justify-content-center mt-3">
+                                            <button className="sendButton" type="submit">Send</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </article>
             </section>
         )
