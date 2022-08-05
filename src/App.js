91da48ed4f047c72603dc97f525a0913a00f310c
AngelMond <abraham_will@hotmail.com>
2022-08-05 04:28:24 -0500

    Loading routes from App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './styles.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-
-
-import RouterComponent from './Router';
+// import Carousel from './components/carousel/Carousel.js';
+import HomePage from './components/home/HomePage';
+import AboutMe from './components/about-me/AboutMe'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+import Resume from './components/resume/Resume'
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <main className='container'>
-      <RouterComponent/>
+        <Router>
+          <Routes>
+            <Route path='/' element={<HomePage/>}/>
+            <Route path='/about' element={<AboutMe/>}/>
+            <Route path='/portfolio' element={<Portfolio/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/resume' element={<Resume/>}/>
+          </Routes>
+        </Router>
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/Router.js
+++ src/Router.js
@@ -1,24 +1,24 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-// import Carousel from './components/carousel/Carousel.js';
-import HomePage from './components/home/HomePage';
-import AboutMe from './components/about-me/AboutMe'
-import Portfolio from './components/portfolio/Portfolio'
-import Contact from './components/contact/Contact'
-import Resume from './components/resume/Resume'
-class RouterComponent extends Component {
-    render(){
-        return(
-            <Router>
-                <Routes>
-                    <Route path='/' element={<HomePage/>}/>
-                    <Route path='/about' element={<AboutMe/>}/>
-                    <Route path='/portfolio' element={<Portfolio/>}/>
-                    <Route path='/contact' element={<Contact/>}/>
-                    <Route path='/resume' element={<Resume/>}/>
-                </Routes>
-            </Router>
-        )
-    }
-}
-export default RouterComponent;
+// import React, {Component} from 'react';
+// import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+// // import Carousel from './components/carousel/Carousel.js';
+// import HomePage from './components/home/HomePage';
+// import AboutMe from './components/about-me/AboutMe'
+// import Portfolio from './components/portfolio/Portfolio'
+// import Contact from './components/contact/Contact'
+// import Resume from './components/resume/Resume'
+// class RouterComponent extends Component {
+//     render(){
+//         return(
+//             <Router>
+//                 <Routes>
+//                     <Route path='/' element={<HomePage/>}/>
+//                     <Route path='/about' element={<AboutMe/>}/>
+//                     <Route path='/portfolio' element={<Portfolio/>}/>
+//                     <Route path='/contact' element={<Contact/>}/>
+//                     <Route path='/resume' element={<Resume/>}/>
+//                 </Routes>
+//             </Router>
+//         )
+//     }
+// }
+// export default RouterComponent;
