2aed87f7f5f67dbb457df6d2905b04b333ea8e58
AngelMond <abraham_will@hotmail.com>
2022-08-05 04:51:42 -0500

    Returning routes to file Routes.js and added /ReactPortfolio/ route to all routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RouterComponent from './Router'
 import './styles.css';
+
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-// import Carousel from './components/carousel/Carousel.js';
-import HomePage from './components/home/HomePage';
-import AboutMe from './components/about-me/AboutMe'
-import Portfolio from './components/portfolio/Portfolio'
-import Contact from './components/contact/Contact'
-import Resume from './components/resume/Resume'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main className='container'>
-        <Router>
-          <Routes>
-            <Route path='/ReactPortfolio/' element={<HomePage/>}/>
-            <Route path='/ReactPortfolio/about' element={<AboutMe/>}/>
-            <Route path='/ReactPortfolio/portfolio' element={<Portfolio/>}/>
-            <Route path='/ReactPortfolio/contact' element={<Contact/>}/>
-            <Route path='/ReactPortfolio/resume' element={<Resume/>}/>
-          </Routes>
-        </Router>
+          <RouterComponent/>
       </main>
       <Footer />
     </div>
