735ea5c2be7896791e92863521645fd9ae95fce7
AngelMond <abraham_will@hotmail.com>
2022-08-06 14:24:44 -0500

    Changed anchor tags for Link react property

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
     render() {
         return (
             <nav className='menu col-12 col-md-12 col-lg-6 d-flex flex-wrap'>
-                <a className='col-3 navLinks bgLink-1 p-4 text-center text-white' href='/about'>
+                <Link className='col-3 navLinks bgLink-1 p-4 text-center text-white' to='/about'>
                     <span className="fs-md-5">About Me</span>
                     <div className='icon'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
                             <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
                         </svg>
                     </div>
-                </a>
-                <a className='col-3 navLinks bgLink-2 p-4 text-center text-white align-items-center' href='/portfolio'>
+                </Link>
+                <Link className='col-3 navLinks bgLink-2 p-4 text-center text-white align-items-center' to='/portfolio'>
                     <span className="fs-md-5">Portfolio</span>
                     <div className='icon'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-briefcase-fill" viewBox="0 0 16 16">
@@ -20,23 +21,23 @@ class Navbar extends Component {
                             <path d="M0 12.5A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5V6.85L8.129 8.947a.5.5 0 0 1-.258 0L0 6.85v5.65z" />
                         </svg>
                     </div>
-                </a>
-                <a className='col-3 navLinks bgLink-3 p-4 text-center text-white' href='/contact'>
+                </Link>
+                <Link className='col-3 navLinks bgLink-3 p-4 text-center text-white' to='/contact'>
                     <span className="fs-md-5">Contact</span>
                     <div className='icon'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-envelope-fill" viewBox="0 0 16 16">
                             <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
                         </svg>
                     </div>
-                </a>
-                <a className='col-3 navLinks bgLink-4 p-4 text-center text-white' href='/resume'>
+                </Link>
+                <Link className='col-3 navLinks bgLink-4 p-4 text-center text-white' to='/resume'>
                     <span className="fs-md-5">Resume</span>
                     <div className='icon'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-file-earmark-person-fill" viewBox="0 0 16 16">
                             <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm2 5.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-.245S4 12 8 12s5 1.755 5 1.755z" />
                         </svg>
                     </div>
-                </a>
+                </Link>
             </nav>
         )
     }
