423cc01559ce740cdf82cdeda3e3bb96c8f6145c
AngelMond <abraham_will@hotmail.com>
2022-08-08 18:08:31 -0500

    Added className to align text for p tags in aboutme section

--- src/components/about-me/AboutMe.js
+++ src/components/about-me/AboutMe.js
@@ -13,20 +13,20 @@ class AboutMe extends Component {
                             my self and i've worked with different teams in different projects.
                         </p>
 
-                        <p className='paragraph fs-4 px-3 mb-4'>
+                        <p className='paragraph fs-4 px-3 mb-4 text-sm-center text-md-start'>
                             I've always had a passion for technology, i just can sitdown for hours and hours to programm and 
                             i have never felt so much passion for something in my life i'am just happy to have discovered my passion.
                             I remember a saying:  <cite>"Choose a job you're passionate about and you won't have to work a day in your life". </cite>
                             I can say that i really feel that!
                         </p>
 
-                        <p className='fs-4 px-3 mb-4'>
+                        <p className='fs-4 px-3 mb-4 text-sm-center text-md-start'>
                             I consider myself a disciplined person, and that helps me a lot to get the necessary knowledge to work as a web developer. 
                             I really like learning about data structures and algorithms, I think that's a fundamental part of being a good software developer.
                             Also I'm curiuos about cyber security but that something I would like to learn in the future.
                         </p>
 
-                        <p className='paragraph fs-4 px-3 mb-4'>
+                        <p className='paragraph fs-4 px-3 mb-4 text-sm-center text-md-start'>
                             My resume is provided, so please feel free to download it, if interested look forward to hearing from you!
                         </p>
                         <p>
