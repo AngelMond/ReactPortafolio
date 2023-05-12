c7e064c9974066b9d6e25f10b7f44d8d495e599f
AngelMondragon <angel.mondragon@4n.com.mx>
2023-05-11 19:00:48 -0600

    Changed title for cloud engineer image and addded link to go to credentials google page

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 //Frontend Images
 import api from '../../images/tech-images/front-end/api.png';
@@ -23,7 +24,7 @@ import git from '../../images/tech-images/devops/git.png';
 import github from '../../images/tech-images/devops/github.png';
 import heroku from '../../images/tech-images/devops/heroku.png';
 import vscode from '../../images/tech-images/devops/vscode.png';
-import googleEngineer  from '../../images/tech-images/devops/cloudEngineer.png';
+import googleEngineer from '../../images/tech-images/devops/cloudEngineer.png';
 
 //Resume image
 import pdfImage from '../../images/pdf-icon.AVIF'
@@ -36,42 +37,44 @@ class Resume extends Component {
                     <div className="col-md-10 mb-5">
                         <h3 className="text-center border-bottom border-muted p-2 section-headings">FRONT-END PROFICIENCIES</h3>
                         <div className='d-flex flex-wrap justify-content-center p-3'>
-                            <img className="techImagesSize" src={api} alt="API" title="API"/>
-                            <img src={bootstrap} alt="Bootstrap" title="Bootstrap"/>
-                            <img src={css} alt="CSS" title="CSS"/>
-                            <img src={html} alt="HTML" title="HTML"/>
-                            <img src={javascript} alt="JavaScript" title="JavaScript"/>
-                            <img src={jquery} alt="JQuery" title="JQuery"/>
-                            <img src={json} alt="Json" title="Json"/>
-                            <img src={react} alt="React" title="React"/>
+                            <img className="techImagesSize" src={api} alt="API" title="API" />
+                            <img src={bootstrap} alt="Bootstrap" title="Bootstrap" />
+                            <img src={css} alt="CSS" title="CSS" />
+                            <img src={html} alt="HTML" title="HTML" />
+                            <img src={javascript} alt="JavaScript" title="JavaScript" />
+                            <img src={jquery} alt="JQuery" title="JQuery" />
+                            <img src={json} alt="Json" title="Json" />
+                            <img src={react} alt="React" title="React" />
                         </div>
                     </div>
                     <div className="col-md-10 mb-5">
                         <h3 className="text-center border-bottom border-muted p-2 section-headings">BACK-END PROFICIENCIES</h3>
-                            <div className='d-flex flex-wrap justify-content-center p-3'>
-                                <img src={mongodb} alt="MongoDB" title="MongoDB" />
-                                <img src={nodejs} alt="Nodejs" title="Nodejs" />
-                                <img src={npm} alt="NPM" title="NPM" />
-                                <img src={sql} alt="SQL" title="SQL" />
-                                <img src={graphql} alt="Graphql" title="GraphQL" />
-                            </div>
+                        <div className='d-flex flex-wrap justify-content-center p-3'>
+                            <img src={mongodb} alt="MongoDB" title="MongoDB" />
+                            <img src={nodejs} alt="Nodejs" title="Nodejs" />
+                            <img src={npm} alt="NPM" title="NPM" />
+                            <img src={sql} alt="SQL" title="SQL" />
+                            <img src={graphql} alt="Graphql" title="GraphQL" />
+                        </div>
                     </div>
                     <div className="col-md-10 mb-5">
                         <h3 className="text-center border-bottom border-muted p-2 section-headings">DEVOPS/OTHER APPLICATIONS</h3>
-                            <div className='d-flex flex-wrap justify-content-center p-3'>
-                                <img src={git} alt="Git" title="Git" />
-                                <img src={github} alt="Github" title="Github" />
-                                <img src={heroku} alt="Heroku" title="Heroku" />
-                                <img src={vscode} alt="VSCode" title="VSCode" />
-                                <img src={googleEngineer} alt="Google Associate Cloud Engineer" title="Google Associate Cloud Engineer" className="resumeImages"  />
-                            </div>
+                        <div className='d-flex flex-wrap justify-content-center p-3'>
+                            <a href="https://google.accredible.com/88844f39-e85b-4985-83d3-a99c89bbb6a8" target="_blank">
+                                <img src={googleEngineer} alt="Google Associate Cloud Engineer" title="Clic to view credentials as Associate Cloud Engineer" className="resumeImages" />
+                            </a>
+                            <img src={git} alt="Git" title="Git" />
+                            <img src={github} alt="Github" title="Github" />
+                            <img src={heroku} alt="Heroku" title="Heroku" />
+                            <img src={vscode} alt="VSCode" title="VSCode" />
+                        </div>
                     </div>
                     <div className="col-md-10 mb-5">
                         <h3 className="text-center border-bottom border-muted p-2 section-headings">DOWNLOAD RESUME</h3>
-                            <div className='d-flex flex-wrap justify-content-center p-3'>
-                                <a className="resumeImage" href={resume} target="_blank" rel="noopener noreferrer"><img src={pdfImage} width="50" height="50" alt="resume" title="pdf-resume" /></a>
-                                <p className="col-12 text-center text-muted mt-2" id="titleResume">Click to Download</p>
-                            </div>
+                        <div className='d-flex flex-wrap justify-content-center p-3'>
+                            <a className="resumeImage" href={resume} target="_blank" rel="noopener noreferrer"><img src={pdfImage} width="50" height="50" alt="resume" title="pdf-resume" /></a>
+                            <p className="col-12 text-center text-muted mt-2" id="titleResume">Click to Download</p>
+                        </div>
                     </div>
                 </article>
             </section>
