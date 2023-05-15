434c47d5597e96789e3f1161a11f2ef7f185ce0e
AngelMondragon <angel.mondragon@4n.com.mx>
2023-05-14 21:18:04 -0600

    Corrected sending message api and added environment variables

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,6 +1,10 @@
 import React, { useState, useRef } from "react";
 import emailjs from '@emailjs/browser';
 
+
+
+
+
 const Contact = () => {
     const formRef = useRef();
 
@@ -8,12 +12,15 @@ const Contact = () => {
     const [errorEmail, setErrorEmail] = useState(null);
     const [errorName, setErrorName] = useState(null);
     const [errorMessage, setErrorMessage] = useState(null);
-    const [messageSendedSuccessfully, setMessageSendedSuccessfully] = useState();
+    const [messageSended, setMessageSended] = useState();
 
-    
-    
+    const [isLoading, setIsLoading] = useState(false);
+
+
+
     //Handler for Email input
     const handleValidEmail = event => {
+        setMessageSended(null);
         if (!isValidEmail(event.target.value)) {
             //If input not receive a valid email will display an error message
             setErrorEmail('Email is invalid');
@@ -30,6 +37,7 @@ const Contact = () => {
 
     //Handler for Name input
     const handlerName = (event) => {
+        setMessageSended(null);
         if (!requireName(event.target.value)) {
             //If input not receive any value will display an error message
             setErrorName("This field is required")
@@ -44,12 +52,13 @@ const Contact = () => {
     }
 
 
-    
+
     //Handler for Message input
     const handlerMessage = (event) => {
+        setMessageSended(null);
         if (!requireMessage(event.target.value)) {
             //If input not receive any value will display an error message
-            setErrorMessage("This field is required")
+            setErrorMessage("This field is required");
         } else {
             setErrorMessage(null)
         }
@@ -65,18 +74,22 @@ const Contact = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        emailjs.sendForm('service_9mqxg39', 'template_oy8guul', formRef.current, '6uokKZOsjTF7uLKam')
+        setIsLoading(true);
+        emailjs.sendForm(process.env.REACT_APP_EMAIL_SERVICE, process.env.REACT_APP_EMAIL_ID, formRef.current, process.env.REACT_APP_USER_ID)
             .then((result) => {
-                console.log(result)
-                console.log(result.text);
                 setErrorMessage(false);
-                //Clean inputs
-                document.querySelector("#inputName").value = " ";
-                document.querySelector("#inputEmail").value = " ";
-                document.querySelector("#inputMessage").value = " ";
-                //Display message sended
-                //document.querySelector("#messageSended").style.display = "block";
-                setMessageSendedSuccessfully(true);
+
+                if (result.status === 200) {
+                    setMessageSended("Message sended");
+                    setIsLoading(false);
+
+
+                } else {
+                    setErrorMessage("Message not sent");
+                    setIsLoading(false);
+
+                }
+
             }, (error) => {
                 console.log(error.text);
             });
@@ -90,37 +103,53 @@ const Contact = () => {
                 <div className="row justify-content-center mt-4">
                     <div className="col-12 col-md-9">
                         <form ref={formRef} onSubmit={handleSubmit} id="form" >
-                            <div className="row">
-                                <div className="col-12 col-md-6 mb-3">
-                                    <div className="form-floating ">
-                                        <input onMouseLeave={handlerName} id="inputName" className="form-control" type="text" name="name" placeholder="Name" />
-                                        <label className="text-start" htmlFor="name">Name</label>
-                                        {errorName && <h5 id="requireName" className=" text-danger fs-6 mt-2 text-start">{errorName}</h5>}
-                                    </div>
-                                </div>
-                                <div className="col-12 col-md-6 mb-3">
-                                    <div className="form-floating ">
-                                        <input onChange={handleValidEmail} id="inputEmail" className="form-control" type="text" name="email" placeholder="email" />
-                                        <label className="text-start" htmlFor="email">email</label>
-                                        {errorEmail && <h5 id="invalidEmail" className=" text-danger fs-6 mt-2 text-start">{errorEmail}</h5>}
-                                    </div>
-                                </div>
-                                <div className="col-12">
-                                    <div className="form-floating ">
-                                        <textarea onMouseLeave={handlerMessage} id="inputMessage" className="form-control textarea-contact" name="message" placeholder="Message">
-                                        </textarea>
-                                        <label className="text-start" htmlFor="message">Message</label>
+                            <div className="row mt-3">
+                                {!isLoading &&
+                                    <>
+                                        <div className="col-12 col-md-6 mb-3">
+                                            <div className="form-floating ">
+                                                <input onMouseLeave={handlerName} id="inputName" className="form-control" type="text" name="name" placeholder="Name" />
+                                                <label className="text-start" htmlFor="name">Name</label>
+                                                {errorName && <h5 id="requireName" className=" text-danger fs-6 mt-2 text-start">{errorName}</h5>}
+                                            </div>
+                                        </div>
+                                        <div className="col-12 col-md-6 mb-3">
+                                            <div className="form-floating ">
+                                                <input onChange={handleValidEmail} id="inputEmail" className="form-control" type="text" name="email" placeholder="email" />
+                                                <label className="text-start" htmlFor="email">email</label>
+                                                {errorEmail && <h5 id="invalidEmail" className=" text-danger fs-6 mt-2 text-start">{errorEmail}</h5>}
+                                            </div>
+                                        </div>
+                                        <div className="col-12">
+                                            <div className="form-floating ">
+                                                <textarea onMouseLeave={handlerMessage} id="inputMessage" className="form-control textarea-contact" name="message" placeholder="Message">
+                                                </textarea>
+                                                <label className="text-start" htmlFor="message">Message</label>
+                                                {errorMessage && <h5 id="requireMessage" className=" text-danger fs-6 mt-2 text-start">{errorMessage}</h5>}
+                                                {messageSended &&
+                                                    <h5 className=" text-success fs-6 mt-2 text-center">{messageSended}</h5>
+                                                }
+                                            </div>
 
-                                        {errorMessage && <h5 id="requireMessage" className=" text-danger fs-6 mt-2 text-start">{errorMessage}</h5>}
-                                        {messageSendedSuccessfully &&
-                                            <h5 id="messageSended" className=" text-success fs-6 mt-2 text-center">Message sended</h5>
-                                        }
+                                            <div className="d-flex justify-content-end mt-3">
+                                                <input id="reload" className="sendButton text-start" type="submit" value="Send" />
+                                            </div>
+                                        </div>
+                                    </>
+                                }
+                                {isLoading &&
+                                    <div className="col-12">
+                                        <div className="row justify-content-center">
+                                            <div class="spinner-border col-12" role="status">
+                                            </div>
+                                            <span class="text-center">Sending message...</span>
+
+                                        </div>
                                     </div>
-                                    <div className="d-flex justify-content-end mt-3">
-                                        <input id="reload" className="sendButton text-start" type="submit" value="Send" />
-                                    </div>
-                                </div>
+
+                                }
                             </div>
+
                         </form>
                     </div>
                 </div>
