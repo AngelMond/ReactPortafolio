e24f78fdd19ffcdbb58cfd5a6bee7f9e2a3a0b73
AngelMondragon <angel.mondragon@4n.com.mx>
2023-05-14 19:01:22 -0600

    Updated about me section

--- src/components/about-me/AboutMe.js
+++ src/components/about-me/AboutMe.js
@@ -1,38 +1,83 @@
 import React, { Component } from 'react';
 
+import googleEngineer from '../../images/tech-images/devops/cloudEngineer.png';
+
+
 class AboutMe extends Component {
     render() {
         return (
             <section className='container '>
-                <article  className='section-border mt-5 p-4 shadow'>
-                    <h3 className='paragraph text-center section-headings'>Full Stack Web Developer</h3>
+                <article className='section-border mt-5 p-4 shadow ff text-start'>
+                    <h3 className='paragraph text-center section-headings mb-4'>Full Stack Web Developer</h3>
                     <div id="aboutMe-text-container" className='p-2'>
-                        <p className='paragraph fs-4 px-3 mb-4 text-sm-center text-md-start'>
-                            I'm from Mexico City and i'm almost have a year of experience as a fullstack developer, mostly in the front-end using React and bootstrap.
-                            Experience using Nodejs, and creating user interfaces.
+                        <p className='paragraph fs-4 px-3 mb-4 text-sm-start text-md-start'>
+                            I'm from Mexico City and almost a year of experience as a fullstack developer. front-end using React and bootstrap and integreating APIs created in Java for all the communication to the server.
+                            Experience using Express and Nodejs in general.
                         </p>
-                        <p className='paragraph fs-4 px-3 mb-4 text-sm-center text-md-start fw-bold'>
-                            I'm certified as a Google Associate Cloud Engineer.
+                        <p className='paragraph fs-4 px-3 mb-4 text-sm-start text-md-start fw-bold'>
+                            Experience building +100 user interfaces in the currect project.
                         </p>
-                        <p className='paragraph fs-4 px-3 mb-4 text-sm-center text-md-start fw-bold'>
-                            Experience creating VMs, clusters in kubernetes, use of containers and registry to deploy in Cloud Run, using of images to deploy applications in App Engine.
+                        <p className='paragraph fs-4 px-3 text-sm-start text-md-start fw-bold mb-5 mt-5'>
+                            <div className='row align-items-center'>
+                                <div className='col-12 col-sm-4 col-md-2 '>
+                                    <a href="https://google.accredible.com/88844f39-e85b-4985-83d3-a99c89bbb6a8" target="_blank">
+                                        <img src={googleEngineer} alt="Google Associate Cloud Engineer" title="Click to view credentials as Associate Cloud Engineer" className="aboutMeImages" />
+                                    </a>
+                                </div>
+                                <div className='col-12 col-sm-8 col-md-10'>
+                                    <span className=''>Certified as a Google Associate Cloud Engineer</span>
+                                </div>
+
+                            </div>
+                        </p>
+                        <p className='paragraph fs-4 px-3 mb-4 text-sm-start text-md-start'>
+
+                            <div className='row mt-5'>
+                                <div className='col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+                                    <span className='fw-bold'> Skills as Google Cloud Engineer:</span>
+                                    <ul className='mt-4 mb-5 text-start'>
+                                        <li className='text-muted ff'>Creation of VMs and instance groups</li>
+                                        <li className='text-muted ff'>Clusters in kubernetes</li>
+                                        <li className='text-muted ff'>Docker &#40;Containers & Registry&#41;</li>
+                                        <li className='text-muted ff'>Deploy applications using Cloud BUild, Cloud Run and App Engine</li>
+                                        <li className='text-muted ff'>CI/CD - Splitting traffic across different versions in App Engine</li>
+                                    </ul>
+                                </div>
+
+                                <div className='col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+                                    <span className='fw-bold'> Skills as Developer:</span>
+                                    <ul className='mt-4 mb-5 text-start'>
+                                        <li className='text-muted ff'>JavaScript</li>
+                                        <li className='text-muted ff'>
+                                            React
+                                            <ul>
+                                                <li>Professional use of react hooks &#40;useState, useContext, useEffect&#41;</li>
+                                                <li>Conditional rendering</li>
+                                            </ul>
+                                        </li>
+                                        <li className='text-muted ff'>Creation and validation of forms for securities transactions</li>
+                                        <li className='text-muted ff'>TDD &#40;Test Driven Develpment&#41;</li>
+                                        <li className='text-muted ff'>Initialize react with CRA or Vite</li>
+                                    </ul>
+                                </div>
+                            </div>
                         </p>
 
-                        <p className='paragraph fs-4 px-3 mb-4 text-sm-center text-md-start'>
+                        <p className='paragraph fs-4 px-3 mb-4 text-sm-start text-md-start'>
                             I've always had a passion for technology, i just can sitdown for hours and hours to programm. I have never felt so much passion for something in my life and i'am just happy to have discovered my passion.
-                            I remember a saying:  <cite>"Choose a job you're passionate about and you won't have to work a day in your life". </cite>
+                            I remember a saying:  <cite className='fw-bold'>"Choose a job you're passionate about and you won't have to work a day in your life". </cite>
                             I can say that i'm living that!
                         </p>
 
-                        <p className='fs-4 px-3 mb-4 text-sm-center text-md-start'>
-                            I consider myself a disciplined person, and that helps me a lot to get the necessary knowledge to work as a web developer. 
+                        {/* <p className='fs-4 px-3 mb-4 text-sm-start text-md-start'>
+                            I consider myself a disciplined person, and that helps me a lot to get the necessary knowledge to work as a web developer.
                             I really like learning about data structures and algorithms, I think that's a fundamental part of being a good software developer.
-                        </p>
+                        </p> */}
 
-                        <p className='paragraph fs-4 px-3 mb-4 text-sm-center text-md-start'>
+                        <p className='paragraph fs-4 px-3 mb-4 text-sm-start text-md-start'>
                             My resume is provided, so please feel free to download it, if interested look forward to hearing from you!
                         </p>
-                        <p>
+                        <p className='paragraph px-3 mb-4 text-sm-start text-md-start'>
                             Wisdom comes from experience and experience is often the result of a lack of
                             wisdom.
                             <figcaption>
